147757b6f1a9cef8fb5f28b127d2648268bd2ef0
snehaCodes276 <dsnehajareddy@gmail.com>
2025-03-05 16:09:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load environment variables
+ // Load environment variables
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
